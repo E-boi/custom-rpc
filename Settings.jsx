cf5f2ae360e3e64ab4de3b588a618fbb586418b2
E-boi <hiboysismyoufake6789@gmail.com>
2021-04-20 09:57:21 -0400

    plogin

--- Settings.jsx
+++ Settings.jsx
@@ -1,74 +1,111 @@
-const { React } = require("powercord/webpack");
-const { TextInput, SwitchItem } = require("powercord/components/settings");
-const { Button } = require("powercord/components");
-const path = require("path");
+const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
+const { TextInput, SwitchItem } = require('powercord/components/settings');
+const path = require('path');
 
-module.exports = ({ getSetting, updateSetting, toggleSetting }) => (
-  <div>
-    <SwitchItem
-      note="Toggle RPC using the settings below."
-      value={getSetting("rpcEnabled", true)}
-      onChange={async () => {
-          await toggleSetting("rpcEnabled");
-          let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());
-          if(getSetting("rpcEnabled") == true) {
-            thisPlugin.rpc.enable();
-          } else {
-            thisPlugin.rpc.disable();
-          }
-        }
-      }
-    >
-      Toggle RPC
-    </SwitchItem>
-    
-    <TextInput
-      //note="RPC Application ID"
-      defaultValue={getSetting("rpcId", "711416957718757418")}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcId",val)}}
-    >
-      ID
-    </TextInput>
-    <TextInput
-      //note="RPC Name"
-      defaultValue={getSetting("rpcName", "Lightcord RPC")}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcName",val)}}
-    >
-      Name
-    </TextInput>
-    <TextInput
-      //note="RPC State"
-      defaultValue={getSetting("rpcState", "Powercord Client")}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcState",val)}}
-    >
-      State
-    </TextInput>
-    <TextInput
-      //note="RPC Details"
-      defaultValue={getSetting("rpcDetails", "Browsing Discord")}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcDetails",val)}}
-    >
-      Details
-    </TextInput>
-    <TextInput
-      //note="RPC Small asset ID"
-      defaultValue={getSetting("rpcSmallAsset", null)}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcSmallAsset",val)}}
-    >
-      Small asset ID
-    </TextInput>
-    <TextInput
-      //note="RPC Large asset ID"
-      defaultValue={getSetting("rpcLargeAsset", "757229873071784046")}
-      required={true}
-      onChange={val => {let thisPlugin = powercord.pluginManager.get(__dirname.split(path.sep).pop());thisPlugin.rpc.refresh(true); updateSetting("rpcLargeAsset",val)}}
-    >
-      Large asset ID
-    </TextInput>
-  </div>
-);
+module.exports = class RPCSettings extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = { category0Opened: false, category1Opened: false };
+	}
+
+	render() {
+		const { getSetting, toggleSetting, updateSetting } = this.props;
+		const { getCurrentUser } = getModule(['getCurrentUser'], false);
+		const UserPopout = getModuleByDisplayName('UserProfile', false);
+		return (
+			<div>
+				<SwitchItem
+					value={getSetting('show_time', true)}
+					onChange={() => {
+						toggleSetting('show_time');
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Show Time
+				</SwitchItem>
+				<TextInput defaultValue={getSetting('client_id', '711416957718757418')} required={true} onChange={val => updateSetting('rpcId', val)}>
+					Client ID
+				</TextInput>
+				<TextInput
+					defaultValue={getSetting('name', 'Custom RPC')}
+					onChange={val => {
+						updateSetting('name', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Name
+				</TextInput>
+				<TextInput
+					defaultValue={getSetting('details', 'Browsing Discord')}
+					onChange={val => {
+						updateSetting('details', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Details
+				</TextInput>
+				<TextInput
+					defaultValue={getSetting('state', 'Powercord Client')}
+					onChange={val => {
+						updateSetting('state', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					State
+				</TextInput>
+				<TextInput
+					note="this will be the file name"
+					defaultValue={getSetting('large_image', '')}
+					onChange={val => {
+						updateSetting('large_image', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Large Image
+				</TextInput>
+				<TextInput
+					note="this will be the file name"
+					defaultValue={getSetting('small_image', '')}
+					onChange={val => {
+						updateSetting('small_image', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Small Image
+				</TextInput>
+				<TextInput
+					note="This will show text when large image is hovered"
+					defaultValue={getSetting('large_text', '')}
+					onChange={val => {
+						updateSetting('large_text', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Large Text
+				</TextInput>
+				<TextInput
+					note="This will show text when small image is hovered"
+					defaultValue={getSetting('small_text', '')}
+					onChange={val => {
+						updateSetting('small_text', val);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+				>
+					Small Text
+				</TextInput>
+				<p className="h5-18_1nd">Your popout:</p>
+				<UserPopout user={getCurrentUser()} />
+				<br />
+				<a
+					onClick={() => {
+						require('electron').shell.openExternal(
+							'https://www.reddit.com/r/discordapp/comments/a2c2un/how_to_setup_a_custom_discord_rich_presence_for/'
+						);
+					}}
+				>
+					Help for custom RPC but do step 3 with this plugin
+				</a>
+			</div>
+		);
+	}
+};
