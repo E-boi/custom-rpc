303600da10d0488ec0eb3ccba5a9a461c7a8209a
E-boi <hiboysismyoufake6789@gmail.com>
2021-04-24 13:12:14 -0400

    have saved configs mostly works

--- Settings.jsx
+++ Settings.jsx
@@ -1,88 +1,136 @@
 const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
-const { TextInput, SwitchItem, Category } = require('powercord/components/settings');
+const { TextInput, SwitchItem, Category, SelectInput } = require('powercord/components/settings');
 const path = require('path');
 
+const configs = [
+	{
+		label: 'RPC 1',
+		value: 'rpc1',
+	},
+	{
+		label: 'RPC 2',
+		value: 'rpc2',
+	},
+	{
+		label: 'RPC 3',
+		value: 'rpc3',
+	},
+];
 module.exports = class RPCSettings extends React.PureComponent {
 	constructor(props) {
 		super(props);
-		this.state = { category0Opened: false, category1Opened: false, button1Def: {label: "powercord.dev", url: "https://powercord.dev"}, button2Def: {label: "", url: ""}};
+		this.state = {
+			category0Opened: false,
+			category1Opened: false,
+			button1Def: { label: 'powercord.dev', url: 'https://powercord.dev' },
+			button2Def: { label: '', url: '' },
+			selectedRPC: this.props.getSetting('selected', 'rpc1'),
+		};
+	}
+
+	forceUpdateHandler() {
+		this.forceUpdate();
 	}
 
 	render() {
-		const { getSetting, toggleSetting, updateSetting } = this.props;
+		const { getSetting, updateSetting } = this.props;
 		const { getCurrentUser } = getModule(['getCurrentUser'], false);
 		const UserPopout = getModuleByDisplayName('UserProfile', false);
+		const rpc = getSetting(this.state.selectedRPC);
+		console.log(this.state.selectedRPC);
 		return (
 			<div>
+				<SelectInput
+					searchable={false}
+					value={getSetting('selected', 'rpc1')}
+					onChange={val => {
+						updateSetting('selected', val.value);
+						this.setState({ selectedRPC: val.value });
+						// reeee how to reload
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
+					options={configs.map(({ label, value }) => ({
+						label,
+						value,
+					}))}
+				>
+					RPC CONFIGS
+				</SelectInput>
 				<SwitchItem
-					value={getSetting('show_time', true)}
+					value={getSetting(`${this.state.selectedRPC}.show_time`, true)}
 					onChange={() => {
-						toggleSetting('show_time');
+						rpc.show_time = !rpc.show_time;
 						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 					}}
 				>
 					Show Time
 				</SwitchItem>
 				<TextInput
-					defaultValue={getSetting('client_id', '711416957718757418')}
+					defaultValue={getSetting(`${this.state.selectedRPC}.client_id`, '711416957718757418')}
 					required={true}
 					onChange={val => {
-						updateSetting('client_id', val);
+						rpc.client_id = val;
+						updateSetting(this.state.selectedRPC, rpc);
 						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 					}}
 				>
 					Client ID
 				</TextInput>
 				<TextInput
-					defaultValue={getSetting('start_time', '')}
+					defaultValue={getSetting(`${this.state.selectedRPC}.start_time`, '')}
 					onChange={val => {
-						      updateSetting('start_time', val);
-						      powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC()
-				     	}}
+						rpc.start_time = val;
+						updateSetting(this.state.selectedRPC, rpc);
+						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
+					}}
 				>
 					Custom Start Time In Minutes
 				</TextInput>
 				<TextInput
-					defaultValue={getSetting('name', 'Custom RPC')}
+					defaultValue={getSetting(`${this.state.selectedRPC}.name`)}
 					onChange={val => {
-						updateSetting('name', val);
+						rpc.name = val;
+						updateSetting(this.state.selectedRPC, rpc);
 						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 					}}
 				>
 					Name
 				</TextInput>
 				<TextInput
-					defaultValue={getSetting('details', 'Browsing Discord')}
+					defaultValue={getSetting(`${this.state.selectedRPC}.details`, 'Browsing Discord')}
 					onChange={val => {
-						updateSetting('details', val);
+						rpc.details = val;
+						updateSetting(this.state.selectedRPC, rpc);
 						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 					}}
 				>
 					Details
 				</TextInput>
 				<TextInput
-					defaultValue={getSetting('state', 'Powercord Client')}
+					defaultValue={getSetting(`${this.state.selectedRPC}.state`, 'Powercord Client')}
 					onChange={val => {
-						updateSetting('state', val);
+						rpc.state = val;
+						updateSetting(this.state.selectedRPC, rpc);
 						powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 					}}
 				>
 					State
 				</TextInput>
 				<Category
-                	name='Images'
-                	description={'Manage large and small image appearance'}
-                	opened={this.state.category0Opened}
-            		onChange={() => {
+					name="Images"
+					description={'Manage large and small image appearance'}
+					opened={this.state.category0Opened}
+					onChange={() => {
 						this.setState({ category1Opened: false });
-						this.setState({ category0Opened: !this.state.category0Opened })
+						this.setState({ category0Opened: !this.state.category0Opened });
 					}}
-                >
+				>
 					<TextInput
 						note="this will be the file name"
-						defaultValue={getSetting('large_image', 'powercord')}
+						defaultValue={getSetting(`${this.state.selectedRPC}.large_image`, 'powercord')}
 						onChange={val => {
-							updateSetting('large_image', val);
+							rpc.large_image = val;
+							updateSetting(this.state.selectedRPC, rpc);
 							powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 						}}
 					>
@@ -90,9 +138,10 @@ module.exports = class RPCSettings extends React.PureComponent {
 					</TextInput>
 					<TextInput
 						note="This will show text when large image is hovered"
-						defaultValue={getSetting('large_text', '')}
+						defaultValue={getSetting(`${this.state.selectedRPC}.large_text`, '')}
 						onChange={val => {
-							updateSetting('large_text', val);
+							rpc.large_text = val;
+							updateSetting(this.state.selectedRPC, rpc);
 							powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 						}}
 					>
@@ -100,9 +149,10 @@ module.exports = class RPCSettings extends React.PureComponent {
 					</TextInput>
 					<TextInput
 						note="this will be the file name"
-						defaultValue={getSetting('small_image', 'powercord')}
+						defaultValue={getSetting(`${this.state.selectedRPC}.small_image`, 'powercord')}
 						onChange={val => {
-							updateSetting('small_image', val);
+							rpc.small_image = val;
+							updateSetting(this.state.selectedRPC, rpc);
 							powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 						}}
 					>
@@ -110,9 +160,10 @@ module.exports = class RPCSettings extends React.PureComponent {
 					</TextInput>
 					<TextInput
 						note="This will show text when small image is hovered"
-						defaultValue={getSetting('small_text', '')}
+						defaultValue={getSetting(`${this.state.selectedRPC}.small_text`, '')}
 						onChange={val => {
-							updateSetting('small_text', val);
+							rpc.small_text = val;
+							updateSetting(this.state.selectedRPC, rpc);
 							powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 						}}
 					>
@@ -120,21 +171,20 @@ module.exports = class RPCSettings extends React.PureComponent {
 					</TextInput>
 				</Category>
 				<Category
-                  name='Buttons'
-                  description={'Manage buttons'}
-                  opened={this.state.category1Opened}
-                  onChange={() => {
-					  this.setState({ category0Opened: false });
-					  this.setState({ category1Opened: !this.state.category1Opened });
+					name="Buttons"
+					description={'Manage buttons'}
+					opened={this.state.category1Opened}
+					onChange={() => {
+						this.setState({ category0Opened: false });
+						this.setState({ category1Opened: !this.state.category1Opened });
 					}}
-                >
+				>
 					<TextInput
-						defaultValue={getSetting('button1', this.state.button1Def).label}
+						defaultValue={getSetting(`${this.state.selectedRPC}.button1`, this.state.button1Def).label}
 						onChange={val => {
-							let button1 = getSetting('button1', this.state.button1Def);
-							button1.label = val;
-							updateSetting('button1', button1);
-							if (button1.url != "") {
+							rpc.button1.label = val;
+							updateSetting(this.state.selectedRPC, rpc);
+							if (rpc.button1.url != '') {
 								powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 							}
 						}}
@@ -142,12 +192,11 @@ module.exports = class RPCSettings extends React.PureComponent {
 						Button 1 Text
 					</TextInput>
 					<TextInput
-						defaultValue={getSetting('button1', this.state.button1Def).url}
+						defaultValue={getSetting(`${this.state.selectedRPC}.button1`, this.state.button1Def).url}
 						onChange={val => {
-							let button1 = getSetting('button1', this.state.button1Def);
-							button1.url = val;
-							updateSetting('button1', button1);
-							if (button1.label != "") {
+							rpc.button1.url = val;
+							updateSetting(this.state.selectedRPC, rpc);
+							if (rpc.button1.label != '') {
 								powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 							}
 						}}
@@ -155,12 +204,12 @@ module.exports = class RPCSettings extends React.PureComponent {
 						Button 1 Url
 					</TextInput>
 					<TextInput
-						defaultValue={getSetting('button2', this.state.button2Def).label}
+						defaultValue={getSetting(`${this.state.selectedRPC}.button2`, this.state.button2Def).label}
 						onChange={val => {
-							let button2 = getSetting('button2', this.state.button2Def);
-							button2.label = val;
-							updateSetting('button2', button2);
-							if (button2.url != "") {
+							if (!rpc.button2) rpc.button2 = {};
+							rpc.button2.label = val;
+							updateSetting(this.state.selectedRPC, rpc);
+							if (rpc.button2.url != '') {
 								powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 							}
 						}}
@@ -168,12 +217,12 @@ module.exports = class RPCSettings extends React.PureComponent {
 						Button 2 Text
 					</TextInput>
 					<TextInput
-						defaultValue={getSetting('button2', this.state.button2Def).url}
+						defaultValue={getSetting(`${this.state.selectedRPC}.button2`, this.state.button2Def).url}
 						onChange={val => {
-							let button2 = getSetting('button2', this.state.button2Def);
-							button2.url = val;
-							updateSetting('button2', button2);
-							if (button2.label != "") {
+							if (!rpc.button2) rpc.button2 = {};
+							rpc.button2.url = val;
+							updateSetting(this.state.selectedRPC, rpc);
+							if (rpc.button2.label != '') {
 								powercord.pluginManager.get(__dirname.split(path.sep).pop()).reloadRPC();
 							}
 						}}
--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const { Plugin } = require('powercord/entities');
 const { getModule, React } = require('powercord/webpack');
 const Settings = require('./Settings');
 
+const defaults = {
+	show_time: true,
+	client_id: '711416957718757418',
+	name: 'Custom RPC',
+	state: 'Powercord client',
+	details: 'Browsing Discord',
+	large_image: 'powercord',
+	small_image: 'powercord',
+	button1: {
+		label: 'powercord.dev',
+		url: 'https://powercord.dev/',
+	},
+};
+
 module.exports = class customRPC extends Plugin {
 	reloadRPC() {
+		const selectedRPC = this.settings.get('selected', 'rpc1');
 		const { SET_ACTIVITY } = getModule(['INVITE_BROWSER'], false);
 		SET_ACTIVITY.handler({
 			socket: {
 				id: 100,
 				application: {
-					id: this.settings.get('client_id', '711416957718757418'),
-					name: this.settings.get('name', 'Custom RPC'),
+					id: this.settings.get(`${selectedRPC}.client_id`, '711416957718757418'),
+					name: this.settings.get(`${selectedRPC}.name`, 'Custom RPC'),
 				},
 				transport: 'ipc',
 			},
@@ -21,33 +36,76 @@ module.exports = class customRPC extends Plugin {
 		});
 	}
 
+	convertSettingsForConfigs() {
+		if (this.settings.get('rpc1', undefined)) return;
+		const settings = powercord.api.settings._fluxProps(__dirname.split('/').pop()).settings;
+		if (Object.entries(settings).length === 0) {
+			console.log('setting defaults...');
+			this.settings.set('rpc1', defaults);
+			this.settings.set('rpc2', defaults);
+			this.settings.set('rpc3', defaults);
+			return;
+		}
+		const rpc1 = {
+			show_time: settings.show_time || defaults.show_time,
+			client_id: settings.client_id || defaults.client_id,
+			name: settings.name || defaults.name,
+			state: settings.state || defaults.state,
+			details: settings.details || defaults.details,
+			large_image: settings.large_image || defaults.large_image,
+			small_image: settings.small_image || defaults.small_image,
+			button1: {
+				label: settings.button1?.label ?? defaults.button1.label,
+				url: settings.button1?.url ?? defaults.button1.url,
+			},
+		};
+		if (settings.button2) rpc1.button2 = settings.button2;
+		if (settings.start_time) rpc1.start_time = settings.start_time;
+		if (settings.large_text) rpc1.large_text = settings.large_text;
+		if (settings.small_text) rpc1.small_text = settings.small_text;
+		for (const setting of Object.entries(settings)) this.settings.delete(setting[0]);
+		this.settings.set('rpc1', rpc1);
+		this.settings.set('rpc2', defaults);
+		this.settings.set('rpc3', defaults);
+	}
+
 	game() {
+		const selectedRPC = this.settings.get('selected', 'rpc1');
 		let rp = {
-			details: this.settings.get('details', 'Browsing Discord'),
-			state: this.settings.get('state', 'Powercord Client'),
-			timestamps: this.settings.get('show_time', true)
+			details: this.settings.get(`${selectedRPC}.details`, 'Browsing Discord'),
+			state: this.settings.get(`${selectedRPC}.state`, 'Powercord Client'),
+			timestamps: this.settings.get(`${selectedRPC}.show_time`, true)
 				? {
-					start: this.settings.get('start_time', undefined)
-					? new Date(new Date().getTime() - this.settings.get('start_time', undefined) * 60000).getTime()
-					: Date.now(),
+						start: this.settings.get(`${selectedRPC}.start_time`, undefined)
+							? new Date(new Date().getTime() - this.settings.get(`${selectedRPC}.start_time`, undefined) * 60000).getTime()
+							: Date.now(),
 				  }
 				: undefined,
 			assets: {
-				large_image: this.settings.get('large_image', 'powercord'),
-				small_image: this.settings.get('small_image', 'powercord'),
-				large_text: this.settings.get('large_text', undefined) || undefined,
-				small_text: this.settings.get('small_text', undefined) || undefined,
+				large_image: this.settings.get(`${selectedRPC}.large_image`, 'powercord'),
+				small_image: this.settings.get(`${selectedRPC}.small_image`, 'powercord'),
+				large_text: this.settings.get(`${selectedRPC}.large_text`, undefined) || undefined,
+				small_text: this.settings.get(`${selectedRPC}.small_text`, undefined) || undefined,
 			},
 		};
 
 		let buttons = [];
-		if (this.settings.get('button1', {label: "powercord.dev", url: "https://powercord.dev"}).label != "" && this.settings.get('button1', {label: "powercord.dev", url: "https://powercord.dev"}).url != "") buttons.push(this.settings.get('button1', { label: 'powercord.dev', url: 'https://powercord.dev' }));
-		if (this.settings.get('button2', {label: "", url: ""}).label != "" && this.settings.get('button2', {label: "", url: ""}).url != "") buttons.push(this.settings.get('button2'));
+		if (
+			this.settings.get(`${selectedRPC}.button1`, { label: 'powercord.dev', url: 'https://powercord.dev' }).label != '' &&
+			this.settings.get(`${selectedRPC}.button1`, { label: 'powercord.dev', url: 'https://powercord.dev' }).url != ''
+		)
+			buttons.push(this.settings.get(`${selectedRPC}.button1`, { label: 'powercord.dev', url: 'https://powercord.dev' }));
+		if (
+			this.settings.get(`${selectedRPC}.button2`, { label: '', url: '' }).label != '' &&
+			this.settings.get(`${selectedRPC}.button2`, { label: '', url: '' }).url != ''
+		)
+			buttons.push(this.settings.get(`${selectedRPC}.button2`));
 		if (buttons[0]) rp.buttons = buttons;
 		return rp;
 	}
 
 	startPlugin() {
+		this.convertSettingsForConfigs();
 		this.reloadRPC = this.reloadRPC; // this will be used in settings to reload
 		powercord.api.settings.registerSettings(this.entityID, {
 			category: this.entityID,
@@ -61,13 +119,14 @@ module.exports = class customRPC extends Plugin {
 
 		const { SET_ACTIVITY } = getModule(['INVITE_BROWSER'], false);
 		// without it sometimes the rpc wouldn't show
+		const selectedRPC = this.settings.get('selected', 'rpc1');
 		setTimeout(() => {
 			SET_ACTIVITY.handler({
 				socket: {
 					id: 100,
 					application: {
-						id: this.settings.get('client_id', '711416957718757418'),
-						name: this.settings.get('name', 'Custom RPC'),
+						id: this.settings.get(`${selectedRPC}.client_id`, '711416957718757418'),
+						name: this.settings.get(`${selectedRPC}.name`, 'Custom RPC'),
 					},
 					transport: 'ipc',
 				},
